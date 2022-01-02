cea9f1e663853bbe57626776a7b9c710274d44c0
root <nagaraja.hm.0001@gmail.com>
2022-01-02 22:40:44 +0100

    Update: submodule PduR

---
+++ LATEST/PduR.cpp
@@ -0,0 +1,7 @@
+#include "PduR.h"
+
+class_PduR PduR;
+
+void class_PduR::InitFunction(void){
+}
+
---
+++ LATEST/PduR.h
@@ -0,0 +1,9 @@
+#pragma once
+
+class class_PduR{
+   public:
+      void InitFunction(void);
+};
+
+extern class_PduR PduR;
+
