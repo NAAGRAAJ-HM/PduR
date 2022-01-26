87423fab8d09a10672f742199033839f06f5a0ae
root <nagaraja.hm.0001@gmail.com>
2022-01-26 10:42:33 +0100

    Update: submodule PduR

--- LATEST/PduR.cpp
+++ LATEST/PduR.cpp
@@ -5,3 +5,48 @@ class_PduR PduR;
 FUNC(void, PDUR_CODE) class_PduR::InitFunction(void){
 }
 
+FUNC(void, PDUR_CODE) class_PduR::GetVersinInfo(void){
+}
+
+FUNC(void, PDUR_CODE) class_PduR::GetConfigurationId(void){
+}
+
+FUNC(void, PDUR_CODE) class_PduR::DisableRouting(void){
+}
+
+FUNC(void, PDUR_CODE) class_PduR::EnableRouting(void){
+}
+
+FUNC(void, PDUR_CODE) class_PduR::Transmit(void){
+}
+
+FUNC(void, PDUR_CODE) class_PduR::CancelTransmit(void){
+}
+
+FUNC(void, PDUR_CODE) class_PduR::CancelReceive(void){
+}
+
+FUNC(void, PDUR_CODE) class_PduR::RxIndication(void){
+}
+
+FUNC(void, PDUR_CODE) class_PduR::TxConfirmation(void){
+}
+
+FUNC(void, PDUR_CODE) class_PduR::TriggerTrasnmit(void){
+}
+
+FUNC(void, PDUR_CODE) class_PduR::TpCopyRxData(void){
+}
+
+FUNC(void, PDUR_CODE) class_PduR::TpRxIndication(void){
+}
+
+FUNC(void, PDUR_CODE) class_PduR::TpStartOfReception(void){
+}
+
+FUNC(void, PDUR_CODE) class_PduR::TpCopyTxData(void){
+}
+
+FUNC(void, PDUR_CODE) class_PduR::TpTxConfirmation(void){
+}
+
--- LATEST/PduR.h
+++ LATEST/PduR.h
@@ -5,7 +5,22 @@
 
 class class_PduR{
    public:
-      FUNC(void, PDUR_CODE) InitFunction(void);
+      FUNC(void, PDUR_CODE) InitFunction       (void);
+      FUNC(void, PDUR_CODE) GetVersinInfo      (void);
+      FUNC(void, PDUR_CODE) GetConfigurationId (void);
+      FUNC(void, PDUR_CODE) DisableRouting     (void);
+      FUNC(void, PDUR_CODE) EnableRouting      (void);
+      FUNC(void, PDUR_CODE) Transmit           (void);
+      FUNC(void, PDUR_CODE) CancelTransmit     (void);
+      FUNC(void, PDUR_CODE) CancelReceive      (void);
+      FUNC(void, PDUR_CODE) RxIndication       (void);
+      FUNC(void, PDUR_CODE) TxConfirmation     (void);
+      FUNC(void, PDUR_CODE) TriggerTrasnmit    (void);
+      FUNC(void, PDUR_CODE) TpCopyRxData       (void);
+      FUNC(void, PDUR_CODE) TpRxIndication     (void);
+      FUNC(void, PDUR_CODE) TpStartOfReception (void);
+      FUNC(void, PDUR_CODE) TpCopyTxData       (void);
+      FUNC(void, PDUR_CODE) TpTxConfirmation   (void);
 };
 
 extern class_PduR PduR;
