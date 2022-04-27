24c795a59ff8504b7a9186ea37fffb8cdc3e6d9e
NAGARAJA HM <nagaraja.hm.0001@gmail.com>
2022-04-27 10:45:30 +0000

    Update: AUTOSAR basic software service module PduR

--- LATEST/PduR.cpp
+++ LATEST/PduR.cpp
@@ -31,24 +31,6 @@
 /******************************************************************************/
 /* TYPEDEFS                                                                   */
 /******************************************************************************/
-class class_PduR_Functionality{
-   public:
-      FUNC(void, PDUR_CODE) GetConfigurationId (void);
-      FUNC(void, PDUR_CODE) DisableRouting     (void);
-      FUNC(void, PDUR_CODE) EnableRouting      (void);
-      FUNC(void, PDUR_CODE) Transmit           (void);
-      FUNC(void, PDUR_CODE) CancelTransmit     (void);
-      FUNC(void, PDUR_CODE) CancelReceive      (void);
-      FUNC(void, PDUR_CODE) RxIndication       (void);
-      FUNC(void, PDUR_CODE) TxConfirmation     (void);
-      FUNC(void, PDUR_CODE) TriggerTrasnmit    (void);
-      FUNC(void, PDUR_CODE) TpCopyRxData       (void);
-      FUNC(void, PDUR_CODE) TpRxIndication     (void);
-      FUNC(void, PDUR_CODE) TpStartOfReception (void);
-      FUNC(void, PDUR_CODE) TpCopyTxData       (void);
-      FUNC(void, PDUR_CODE) TpTxConfirmation   (void);
-};
-
 class module_PduR:
       public abstract_module
    ,  public class_PduR_Functionality
