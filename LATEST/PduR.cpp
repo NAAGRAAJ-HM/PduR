7a673f7604e99a4edd6f2259cffdf910b230e1b3
NAGARAJA HM <nagaraja.hm.0001@gmail.com>
2022-05-05 10:09:46 +0000

    Update: AUTOSAR basic software service module PduR

--- LATEST/PduR.cpp
+++ LATEST/PduR.cpp
@@ -85,12 +85,10 @@ FUNC(void, PDUR_CODE) module_PduR::InitFunction(
    if(E_OK != IsInitDone){
 #endif
       if(NULL_PTR != lptrCfgModule){
-         if(STD_LOW){
-            // check lptrCfgModule for memory faults
+         if(STD_HIGH){
             lptrCfg = lptrCfgModule;
          }
          else{
-            // use PBcfgCanIf as back-up configuration
             lptrCfg = &PBcfgPduR;
          }
       }
