4b27e807356750bb7245608bfc0c5ddb33a5d2d5
NAGARAJA HM <nagaraja.hm.0001@gmail.com>
2022-06-29 09:03:52 +0000

    Update: AUTOSAR basic software service module for protocol data unit router

--- LATEST/PduR.cpp
+++ LATEST/PduR.cpp
@@ -41,12 +41,8 @@ class module_PduR:
 /******************************************************************************/
 /* OBJECTS                                                                    */
 /******************************************************************************/
-            Std_TypeReturn          IsInitDone{E_NOT_OK};
-      const CfgModule_TypeAbstract* lptrCfg{(CfgModule_TypeAbstract*)NULL_PTR};
 
    public:
-      module_PduR(Std_TypeVersionInfo lVersionInfo) : abstract_module(lVersionInfo){
-      }
       FUNC(void, PDUR_CODE) InitFunction(
          CONSTP2CONST(CfgModule_TypeAbstract, PDUR_CONFIG_DATA, PDUR_APPL_CONST) lptrCfgModule
       );
@@ -71,18 +67,7 @@ CONSTP2VAR(infSchMClient, PDUR_VAR, PDUR_CONST) gptrinfSchMClient_PduR = &PduR;
 /******************************************************************************/
 /* OBJECTS                                                                    */
 /******************************************************************************/
-VAR(module_PduR, PDUR_VAR) PduR(
-   {
-         PDUR_AR_RELEASE_VERSION_MAJOR
-      ,  PDUR_AR_RELEASE_VERSION_MINOR
-      ,  0x00
-      ,  0xFF
-      ,  0x01
-      ,  '0'
-      ,  '1'
-      ,  '0'
-   }
-);
+VAR(module_PduR, PDUR_VAR) PduR;
 
 /******************************************************************************/
 /* FUNCTIONS                                                                  */
