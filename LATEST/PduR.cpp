58f2d9feb0bdd02d69dab72a6a80e7f8055fde11
NAGARAJA HM <nagaraja.hm.0001@gmail.com>
2022-07-06 17:31:46 +0000

    Update: AUTOSAR basic software service module for protocol data unit router

--- LATEST/PduR.cpp
+++ LATEST/PduR.cpp
@@ -7,9 +7,7 @@
 /* #INCLUDES                                                                  */
 /******************************************************************************/
 #include "Module.hpp"
-#include "CfgPduR.hpp"
-#include "PduR_core.hpp"
-#include "infPduR_Exp.hpp"
+#include "PduR.hpp"
 #include "infPduR_Imp.hpp"
 
 /******************************************************************************/
@@ -32,26 +30,6 @@
 /******************************************************************************/
 /* TYPEDEFS                                                                   */
 /******************************************************************************/
-class module_PduR:
-      INTERFACES_EXPORTED_PDUR
-      public abstract_module
-   ,  public class_PduR_Functionality
-{
-   private:
-/******************************************************************************/
-/* OBJECTS                                                                    */
-/******************************************************************************/
-
-   public:
-      FUNC(void, PDUR_CODE) InitFunction(
-         CONSTP2CONST(CfgModule_TypeAbstract, PDUR_CONFIG_DATA, PDUR_APPL_CONST) lptrCfgModule
-      );
-      FUNC(void, PDUR_CODE) DeInitFunction (void);
-      FUNC(void, PDUR_CODE) MainFunction   (void);
-      PDUR_CORE_FUNCTIONALITIES
-};
-
-extern VAR(module_PduR, PDUR_VAR) PduR;
 
 /******************************************************************************/
 /* CONSTS                                                                     */
