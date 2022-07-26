039d02dae91918974b12d2347e4a7103b87098ac
Nagaraja HM <nagaraja.hm.0001@gmail.com>
2022-07-26 19:51:27 +0200

    Update: AUTOSAR basic software service module for protocol data unit router

--- LATEST/PduR.cpp
+++ LATEST/PduR.cpp
@@ -61,7 +61,7 @@ FUNC(void, PDUR_CODE) module_PduR::InitFunction(
             (NULL_PTR != lptrConstModule)
          && (NULL_PTR != lptrCfgModule)
       ){
-         lptrConst = lptrConstModule;
+         lptrConst = (const ConstPduR_Type*)lptrConstModule;
          lptrCfg   = lptrCfgModule;
       }
       else{
