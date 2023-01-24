ddd632bf512bc325b6507f009ff6daa1451aafad
Nagaraja HM <nagaraja.hm.0001@gmail.com>
2023-01-24 21:50:09 +0100

    Update: AUTOSAR basic software service module for protocol data unit router

---
+++ main_R2023/infServicePduR_ServiceNvM.hpp
@@ -0,0 +1,19 @@
+#pragma once
+/******************************************************************************/
+/* File   : infServicePduR_ServiceNvM.hpp                                     */
+/* Author : NAGARAJA HM (c) since 1982. All rights reserved.                  */
+/******************************************************************************/
+
+/******************************************************************************/
+/* #DEFINES                                                                   */
+/******************************************************************************/
+#define CONSTSERVICEPDUR_AR_RELEASE_VERSION_MAJOR                              4
+#define CONSTSERVICEPDUR_AR_RELEASE_VERSION_MINOR                              3
+
+#define Const_DefaultInit_ConstServicePduR                                                                               \
+   ,  {CONSTSERVICEPDUR_AR_RELEASE_VERSION_MAJOR, CONSTSERVICEPDUR_AR_RELEASE_VERSION_MINOR, 0x00,0xFF,0x01,'0','1','0'} \
+
+/******************************************************************************/
+/* EOF                                                                        */
+/******************************************************************************/
+
