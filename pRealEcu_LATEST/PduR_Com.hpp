6c3d5198ac73859a5a37f05db9df9bfd8076d14c
NagarajaHuliyapuradaMata <nagaraja.hm.0001@gmail.com>
2023-11-20 09:05:01 +0100

    AUTOSAR Migration Activities

--- pRealEcu_LATEST/PduR_Com.hpp
+++
@@ -1,9 +0,0 @@
-#ifndef PDUR_COM_H
-#define PDUR_COM_H
-
-#include "PduR.hpp"
-#include "Com.hpp"
-
-extern FUNC(Std_ReturnType, PDUR_CODE) PduR_ComTransmit( VAR(Type_SwcServiceCom_tIdPdu, AUTOMATIC) id, P2CONST(Type_SwcServiceCom_stInfoPdu, TYPEDEF, PDUR_APPL_DATA) ptr );
-
-#endif
